2986321d0e5049983572ad8c9247b439d509576f
Valera Galeev <71322091+webAmoeba@users.noreply.github.com>
2025-05-18 13:49:21 +0400

    feat: add status creation functionality
    chore: adjust ruff configuration to include migration files

--- task_manager/apps/statuses/urls.py
+++ task_manager/apps/statuses/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from task_manager.apps.statuses.views import (
+    StatusCreateView,
     StatusListView,
 )
 
 urlpatterns = [
     path("", StatusListView.as_view(), name="status_list"),
+    path("create/", StatusCreateView.as_view(), name="status_create"),
 ]
--- task_manager/apps/statuses/views.py
+++ task_manager/apps/statuses/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 
 from task_manager.apps.statuses.models import Status
 
@@ -13,3 +17,11 @@ class StatusListView(ListView):
         context = super().get_context_data(**kwargs)
         context["page_title"] = _("Statuses")
         return context
+
+
+class StatusCreateView(SuccessMessageMixin, CreateView, LoginRequiredMixin):
+    model = Status
+    template_name = "statuses/status_form.html"
+    fields = ["name"]
+    success_url = reverse_lazy("status_list")
+    success_message = _("Status created successfully")
