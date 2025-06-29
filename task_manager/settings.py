d25867ad107ed41c326e857be63bb1cdb2a7edd4
Valera Galeev <71322091+webAmoeba@users.noreply.github.com>
2025-06-29 10:19:25 +0400

    docs: update deployed link in README.md

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", False)
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = ['webserver', '127.0.0.1', 'localhost']
 if os.getenv("webserver"):
