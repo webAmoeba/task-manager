02d3f1fe757f2c6e45c46f87b88e3d54d7845ff3
Valera Galeev <71322091+webAmoeba@users.noreply.github.com>
2025-10-11 09:58:00 +0400

    feat: Add deadline and completed_at fields to tasks
    This commit introduces the following changes:
    
    - Added `due_at` and `completed_at` fields to the Task model.
    - Updated the TaskForm to include a datetime input for the `due_at` field.
    - Modified the task detail and list templates to display the `due_at` and `completed_at` values.

--- task_manager/apps/tasks/forms.py
+++ task_manager/apps/tasks/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
 from task_manager.apps.tasks.models import Task
@@ -15,6 +16,12 @@ class TaskForm(forms.ModelForm):
             "due_at",
             "labels",
         ]
+        widgets = {
+            "due_at": forms.DateTimeInput(
+                attrs={"type": "datetime-local"},
+                format="%Y-%m-%dT%H:%M",
+            ),
+        }
 
     def clean_name(self):
         name = self.cleaned_data["name"]
@@ -24,3 +31,17 @@ class TaskForm(forms.ModelForm):
                 _("Task with this name already exists.")
             )
         return name
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        due_field = self.fields.get("due_at")
+        if due_field:
+            due_field.required = False
+            due_field.input_formats = ["%Y-%m-%dT%H:%M"]
+            if self.instance.pk and self.instance.due_at:
+                original_due = self.instance.due_at
+                if timezone.is_naive(original_due):
+                    local_due = original_due
+                else:
+                    local_due = timezone.localtime(original_due)
+                self.initial["due_at"] = local_due.strftime("%Y-%m-%dT%H:%M")
